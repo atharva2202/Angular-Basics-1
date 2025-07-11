f7d2dd6b3d1ff5e32a6004d1dc0c86a4ec048dc5
atharva2202 <phadkeatharva03@gmail.com>
2025-07-11 13:01:06 +0530

    second commit

---
+++ src/app/pipes/special-char-remover-pipe.spec.ts
@@ -0,0 +1,8 @@
+import { SpecialCharRemoverPipe } from './special-char-remover-pipe';
+
+describe('SpecialCharRemoverPipe', () => {
+  it('create an instance', () => {
+    const pipe = new SpecialCharRemoverPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
